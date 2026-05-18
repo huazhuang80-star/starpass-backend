5784f8c6d456dea8da27026458c35971b09076b7
Michael Victor <158047120+michaelvic123@users.noreply.github.com>
2026-05-18 10:16:24 +0100

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { CreatorsModule } from './creators/creators.module';
+import { FansModule } from './fans/fans.module';
+import { TiersModule } from './tiers/tiers.module';
+import { PassesModule } from './passes/passes.module';
+import { IndexerModule } from './indexer/indexer.module';
+import { StellarModule } from './stellar/stellar.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    CreatorsModule,
+    FansModule,
+    TiersModule,
+    PassesModule,
+    IndexerModule,
+    StellarModule,
+  ],
+})
+export class AppModule {}
